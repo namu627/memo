84f8eebd4947584e290520b8b4b83fbd839ecfea
namu627 <0627eric@naver.com>
2026-08-12 14:36:41 +0900

    first commit

---
+++ src/main/java/com/meta/memo/dto/MemoResponseDto.java
@@ -0,0 +1,17 @@
+package com.meta.memo.dto;
+
+import com.meta.memo.domain.Memo;
+import lombok.Getter;
+
+@Getter
+public class MemoResponseDto {
+    private Long id;
+    private String username;
+    private String contents;
+
+    public MemoResponseDto(Memo memo) {
+        this.id = memo.getId();
+        this.username = memo.getUsername();
+        this.contents = memo.getContents();
+    }
+}
